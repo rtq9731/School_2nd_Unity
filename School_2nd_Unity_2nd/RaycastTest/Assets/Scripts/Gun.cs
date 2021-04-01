335c4228bdd0d49da1f9453b69815e2bda392951
COCONUT <64309783+rtq9731@users.noreply.github.com>
2021-04-01 11:00:35 +0900

    4월 1일 게임엔진응용 2교시

--- School_2nd_Unity_2nd/RaycastTest/Assets/Scripts/Gun.cs
+++ School_2nd_Unity_2nd/RaycastTest/Assets/Scripts/Gun.cs
@@ -10,8 +10,47 @@ public class Gun : MonoBehaviour
     [SerializeField]
     public Camera fpsCam;
 
+    [SerializeField]
+    [Header("파티클 시스템")]
+    public ParticleSystem muzzleFlash;
+
+    [SerializeField]
+    [Header("맞았을 시 발생할 파티클")]
+    public GameObject impactEffect;
+
+    [SerializeField]
+    [Header("총알이 물리적으로 미는 힘")]
+    public float impactFos = 60f;
+
+    [SerializeField] [Header("연사 시 발사 속도")]
+    public float fireRate = 3f;
+    private float nextTimeToFire;
+
+    [SerializeField]
+    [Header("라인 렌더러 / 레이저")]
+    public LineRenderer lineLaser = null;
+    [SerializeField]
+    [Header("크로스헤어")]
+    public GameObject crosshair = null;
+
     private void Update()
     {
+        crosshair.SetActive(true);
+        RaycastHit laserHit;
+        Ray laserRay = new Ray(lineLaser.transform.position, lineLaser.transform.forward);
+        if(Physics.Raycast(laserRay, out laserHit))
+        {
+            //라인 렌더러의 길이 조정
+            lineLaser.SetPosition(1, lineLaser.transform.InverseTransformPoint(laserHit.point));
+            //라인 렌더러 끝점에 조준점 동기화.
+            Vector3 crosshairLocation = Camera.main.WorldToScreenPoint(laserHit.point);
+            crosshair.transform.position = crosshairLocation;
+        }
+        else
+        {
+            crosshair.SetActive(false);
+        }
+
         if(Input.GetButtonDown("Fire1"))
         {
             Shoot();
@@ -20,8 +59,11 @@ public class Gun : MonoBehaviour
 
     private void Shoot()
     {
+
+        muzzleFlash.Play();
+
         RaycastHit hit;
-        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range) )
+        if(Physics.Raycast(lineLaser.transform.position, lineLaser.transform.forward, out hit, range) )
         {
             Debug.Log(hit.transform.name);
 
@@ -31,6 +73,17 @@ public class Gun : MonoBehaviour
             {
                 tb.TakeDamage(power);
             }
+
+            // 맞은 박스를 민다.
+
+            if(hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactFos);
+            }
+
+            //총알이 맞은 지점에 파티클 후 사라지게.
+            GameObject impactObj = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactObj, 2f);
         }
     }
 }
