d8788d9512bdd7871862eef80d926d74b0d2534f
COCONUT <64309783+rtq9731@users.noreply.github.com>
2021-04-24 11:23:10 +0900

    적 리스폰 랜덤 색깔 생성 완성

--- School_2nd_Unity_4th/TestParticle/Assets/Scripts/enemy_etc.cs
+++ School_2nd_Unity_4th/TestParticle/Assets/Scripts/enemy_etc.cs
@@ -6,9 +6,11 @@ public class enemy_etc : MonoBehaviour
 {
     [SerializeField] float HP = 5;
     [SerializeField] float respawnTime = 5f;
+    [SerializeField] Material mat;
 
     private void OnParticleCollision(GameObject other)
     {
+        Debug.Log(other.name);
         Damage();
     }
 
@@ -28,5 +30,9 @@ public class enemy_etc : MonoBehaviour
     void Respawn()
     {
         gameObject.SetActive(true);
+        float r = Random.Range(1, 255);
+        float g = Random.Range(1, 255);
+        float b = Random.Range(1, 255);
+        gameObject.GetComponent<Renderer>().material.color = new Color(r / 255, g / 255, b / 255, 1);
     }
 }
